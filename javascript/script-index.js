febeb9fe99845eea75a235f0632bf94e84b9f11c
johnlee25 <john1997@live.ca>
2020-10-03 00:43:46 -0700

    complete

--- javascript/script-index.js
+++ javascript/script-index.js
@@ -1,25 +1,72 @@
-
-var myPix =[["photos/brother.jpg","Jim", "brother"],["photos/grandchild1.jpg","James", "grandchild1"],["photos/grandchild2.jpg","Jacob","grandchild2"],["photos/neice1.jpg","Janna","neice1"],["photos/neice3.jpg","Joan","neice3"],["photos/neice2.jpg","Jane","neice2"],["photos/nephew1.jpg","John","nephew1"],["photos/nephew2.jpg","Jax","nephew2"],["photos/sister.jpg","Jessica","sister"]];
+var myPix = [["photos/brother.jpg", "Jim", "Brother"], ["photos/grandchild1.jpg", "James", "Grandchild 1"], ["photos/grandchild2.jpg", "Jacob", "Grandchild 2"], ["photos/neice1.jpg", "Janna", "Neice 1"], ["photos/neice2.jpg", "Joan", "Neice 2"], ["photos/neice3.jpg", "Jane", "Neice 3"], ["photos/nephew1.jpg", "John", "Nephew 1"], ["photos/nephew2.jpg", "Jax", "Nephew 2"], ["photos/sister.jpg", "Jessica", "Sister"]];
+var score = 0;
+var counter = 0;
+var answ="";
 
 function choosePic() {
-     var img = "<img id=\'pic\' src=\'";
-     var randomIndex = Math.floor(Math.random() * myPix.length);
-     img += myPix[randomIndex][0];
-     img += "\' alt=\'";
-     img += myPix[randomIndex][2];
-     img += "\'/>"
-     return img;
+    var img = "<img id=\'pic\' src=\'";
+    img += myPix[counter][0];
+    img += "\' alt=\'";
+    img += myPix[counter][2];
+    img += "\'/>"
+    return img;
 }
 
 
-//function addQuestion{
 
-//}
+function addQuestion() {
+    answ = document.getElementById("fname").value;
+    if (answ.toLowerCase() == myPix[counter][1].toLowerCase()) {
+        score++;
+        counter++;
+        document.getElementById("etrname").innerHTML="CORRECT!!!";
+        document.getElementById("fname").value="";
+        document.getElementById("nxtbtn").style.width="100%";
+        document.getElementById("nxtbtn").style.margin="10px 0%";
+    }
+    else {
+        document.getElementById("etrname").innerHTML="Correct name was "+myPix[counter][1];
+        document.getElementById("fname").value="";
+        counter++;
+        document.getElementById("nxtbtn").style.width="50%";
+        document.getElementById("nxtbtn").style.margin="10px 25%";
+    }
+    document.getElementById("fname").style.display="none";
+    document.getElementById("submitbtn").style.display="none";
+    document.getElementById("nxtbtn").style.display="block";
+}
 
-//function hoverImg{
-    
-//}
+function nxtQuestion() {
+    if (counter < 9) {
+        document.getElementById("etrname").innerHTML="Enter Name Here";
+        document.getElementById("fname").style.display="inline-block";
+        document.getElementById("submitbtn").style.display="block";
+        document.getElementById("nxtbtn").style.display="none";
+        document.getElementById("pic").src = myPix[counter][0];
+        document.getElementById("pic").alt = myPix[counter][2];
+    }
+    else{
+        document.getElementById("pic").style.display="none";
+        document.getElementById("hint").style.display="none";
+        document.getElementById("etrname").style.display="none";
+        document.getElementById("nxtbtn").style.display="none";
+        document.getElementById("finalscr").style.display="block"
+        document.getElementById("finalscr").innerHTML="Final Score "+score+"/"+counter;
+        document.getElementById("resetbtn").style.display="block";
+    }
+}
 
-//function clickImg{
-    
-//}
+function resetQuestion() {
+    counter=0;
+    score=0;
+    document.getElementById("finalscr").style.display="none";
+    document.getElementById("pic").style.display="block";
+    document.getElementById("pic").src = myPix[counter][0];
+    document.getElementById("pic").alt = myPix[counter][2];
+    document.getElementById("hint").style.display="block";
+    document.getElementById("etrname").style.display="inline-block";
+    document.getElementById("etrname").innerHTML="Enter Name Here";
+    document.getElementById("fname").style.display="inline-block";
+    document.getElementById("submitbtn").style.display="block";
+    document.getElementById("resetbtn").style.display="none";
+}
